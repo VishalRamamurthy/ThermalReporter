00952772cb396778d0058a32f3e9505de6169520
Vishal <vishalrama1114@gmail.com>
2026-02-22 18:35:31 -0800

    Initial Commit

---
+++ ThermalReporter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThermalReporter
+//
+//  Created by Rahul and Vishal Ramamurthy on 2/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThermalReporter/ThermalReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThermalReporterApp.swift
+//  ThermalReporter
+//
+//  Created by Rahul and Vishal Ramamurthy on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct ThermalReporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
